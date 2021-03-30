5e4b032bf15b70b738a6306b457a91cd07ee3110
Shreya <marsvsjupiter@gmail.com>
2021-03-30 19:48:09 +0530

    Commit changes 2021/03/30 7:47 pm

--- llcbank/src/main/java/com/onlinebanking/llcbank/LlcbankApplication.java
+++ llcbank/src/main/java/com/onlinebanking/llcbank/LlcbankApplication.java
@@ -1,4 +1,4 @@
-package com.llcbank.llcbank;
+package com.onlinebanking.llcbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- llcbank/src/test/java/com/onlinebanking/llcbank/LlcbankApplicationTests.java
+++ llcbank/src/test/java/com/onlinebanking/llcbank/LlcbankApplicationTests.java
@@ -1,4 +1,4 @@
-package com.llcbank.llcbank;
+package com.onlinebanking.llcbank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
