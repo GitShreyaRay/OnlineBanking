807a0df2c0d6694ca7e1bbaf14a2be7861912927
Shreya <marsvsjupiter@gmail.com>
2021-03-30 17:52:19 +0530

    Initial Version 1.0

---
+++ llcbank/src/main/java/com/llcbank/llcbank/LlcbankApplication.java
@@ -0,0 +1,13 @@
+package com.llcbank.llcbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlcbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlcbankApplication.class, args);
+	}
+
+}
---
+++ llcbank/src/test/java/com/llcbank/llcbank/LlcbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llcbank.llcbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlcbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
