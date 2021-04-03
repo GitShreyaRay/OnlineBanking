2ce3fd7387b6fd2a6002ee8bbdcd87afc4a326be
Shreya <marsvsjupiter@gmail.com>
2021-04-03 17:21:49 +0530

    Applied Bcrypt encryption hashing for password

--- llcbank/src/main/java/com/onlinebanking/config/ProjectSecurityConfig.java
+++ llcbank/src/main/java/com/onlinebanking/config/ProjectSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -39,7 +39,7 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	protected PasswordEncoder passwordEncoder( ) {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 	
 
